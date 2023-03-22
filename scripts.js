c41c3fbd984396a6ab82025474861bfde22f7119
Raul <RaulGamboa1995@gmail.com>
2023-03-22 14:16:44 -0500

    create functioning rps function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,39 @@
 function getComputerChoice() {
     let firstInt = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
     if (firstInt === 1) {
-        return "Paper"
+        return "paper"
     } else if (firstInt === 2) {
-        return "Rock"
+        return "rock"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
+
+
+function playRound() {
+    getComputerChoice()
+    let computerSelection = getComputerChoice()
+    console.log("Computer has chosen " + computerSelection + "! now pick urs lmao")
+    let askPlayer = prompt('your options are rock, paper or scissors.')
+    let playerSelection = askPlayer.toLowerCase();
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'tie';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'computer wins';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'player wins';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'player wins';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'tie';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'computer wins';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'computer wins';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'player wins';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'tie';
+    }
+}
+
