c390f9152b65576889c9963210274e250952108d
Raul <RaulGamboa1995@gmail.com>
2023-03-22 13:26:10 -0500

    create getComputerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+function getComputerChoice() {
+    let firstInt = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    if (firstInt === 1) {
+        return "Paper"
+    } else if (firstInt === 2) {
+        return "Rock"
+    } else {
+        return "Scissors"
+    }
+}
